f405364e05f2fb25abbfa77332cfe0b3d7b2e76f
Flame442 <34169552+Flame442@users.noreply.github.com>
2020-01-19 12:03:15 -0500

    Some changes
    -Lists can now be set up to be manageable by specific roles
    -Lists can now be sorted by a specific column
    -Showing row numbers is now hidden by default, enabled by a parameter when running `[p]listmaker show`
    -Showing lists you do not own is now hidden by default, enabled by a parameter when running `[p]listmaker list`

--- listmaker/listmaker.py
+++ listmaker/listmaker.py
@@ -37,7 +37,10 @@ class ListMaker(commands.Cog):
 			lists[list_name] = {
 				'author': ctx.author.id,
 				'columns': column_names,
-				'data': []
+				'data': [],
+				#BELOW ARE NOT GUARANTEED TO BE IN A LIST
+				'roles': [],
+				'sort': None
 			}
 		await ctx.send(f'List `{list_name}` created.')
 		
@@ -48,6 +51,7 @@ class ListMaker(commands.Cog):
 		
 		Specify the list name first and the data for each column after.
 		Wrap anything that requires spaces in quotes.
+		Multiple rows can be added at once by continuing to specify rows.
 		
 		Example:
 		`[p]listmaker add "My friends" "Robert Smith" 26`
@@ -56,13 +60,16 @@ class ListMaker(commands.Cog):
 			if list_name not in lists:
 				await ctx.send('That list does not exist.')
 				return
-			if lists[list_name]['author'] != ctx.author.id:
-				await ctx.send('You do not own that list.')
+			if not self._user_can_access(lists[list_name], ctx.author):
+				await ctx.send('You do not have permission to edit that list.')
 				return
-			if len(values) != len(lists[list_name]['columns']):
+			req_len = len(lists[list_name]['columns'])
+			if len(values) % req_len != 0:
 				await ctx.send('The number of columns provided does not match the number of columns in the list.')
 				return
-			lists[list_name]['data'].append(values)
+			while values:	
+				lists[list_name]['data'].append(values[:req_len])
+				values = values[req_len:]
 		await ctx.send('Data added.')
 	
 	@listmaker.command()
@@ -80,8 +87,8 @@ class ListMaker(commands.Cog):
 			if list_name not in lists:
 				await ctx.send('That list does not exist.')
 				return
-			if lists[list_name]['author'] != ctx.author.id:
-				await ctx.send('You do not own that list.')
+			if not self._user_can_access(lists[list_name], ctx.author):
+				await ctx.send('You do not have permission to edit that list.')
 				return
 			if row_number <= 0:
 				await ctx.send('The row number must be greater than 0.')
@@ -94,11 +101,12 @@ class ListMaker(commands.Cog):
 		await ctx.send('Data removed.')
 	
 	@listmaker.command()
-	async def show(self, ctx, list_name):
+	async def show(self, ctx, list_name, show_index: bool=False):
 		"""
 		View the data of a list.
 		
 		Wrap the list name in quotes if it requires spaces.
+		Use the parameter `show_index` to enable list indexes.
 		
 		Example:
 		`[p]listmaker show "My friends"`
@@ -107,7 +115,13 @@ class ListMaker(commands.Cog):
 		if list_name not in lists:
 			await ctx.send('That list does not exist.')
 			return
-		msg = tabulate(lists[list_name]['data'], headers=lists[list_name]['columns'], showindex=range(1, len(lists[list_name]['data']) + 1))
+		if show_index:
+			show_index = range(1, len(lists[list_name]['data']) + 1)
+		data = lists[list_name]['data']
+		if lists[list_name].get('sort', None):
+			sort = lists[list_name]['sort']
+			data = sorted(data, key=lambda a: a[sort[0]], reverse=sort[1])
+		msg = tabulate(data, headers=lists[list_name]['columns'], showindex=show_index)
 		paged = pagify(msg)
 		box_paged = (f'```{x}```' for x in paged)
 		await ctx.send_interactive(box_paged)
@@ -134,18 +148,85 @@ class ListMaker(commands.Cog):
 		await ctx.send(f'List {list_name} deleted.')
 	
 	@listmaker.command()
-	async def list(self, ctx):
+	async def list(self, ctx, show_all: bool=False):
 		"""
 		List the existing listmaker lists.
 		
 		Also shows the user id of the author of each list.
+		Use the parameter `show_all` to see all lists.
 		"""
 		lists = await self.config.lists()
 		if not lists:
 			await ctx.send('There are currently no lists.')
 			return
-		data = [[name, lists[name]['author']] for name in lists]
+		data = [
+			[name, lists[name]['author']] for name in lists
+			if lists[name]['author'] == ctx.author.id or show_all
+		]
 		msg = tabulate(data, headers=['List Name', 'Author ID'])
 		paged = pagify(msg)
 		box_paged = (f'```{x}```' for x in paged)
 		await ctx.send_interactive(box_paged)
+
+	@listmaker.command()
+	async def role(self, ctx, list_name, role_id: int):
+		"""
+		Toggle if a role_id should be allowed to edit a list.
+		
+		Wrap the list name in quotes if it requires spaces.
+		"""
+		async with self.config.lists() as lists:
+			if list_name not in lists:
+				await ctx.send('That list does not exist.')
+				return
+			if lists[list_name]['author'] != ctx.author.id:
+				await ctx.send('You do not own that list.')
+				return
+			if 'roles' not in lists[list_name]:
+				lists[list_name]['roles'] = [role_id]
+				await ctx.send('Added.')
+				return
+			if role_id in lists[list_name]['roles']:
+				lists[list_name]['roles'].remove(role_id)
+				await ctx.send('Removed.')
+				return
+			lists[list_name]['roles'].append(role_id)
+			await ctx.send('Added.')
+
+	@listmaker.command()
+	async def sort(self, ctx, list_name, column_name=None, reverse: bool=False):
+		"""
+		Set the list to be sorted by a particular column.
+		
+		Wrap anything that requires spaces in quotes.
+		Use the parameter `reverse` to sort the other way.
+		"""
+		async with self.config.lists() as lists:
+			if list_name not in lists:
+				await ctx.send('That list does not exist.')
+				return
+			if not self._user_can_access(lists[list_name], ctx.author):
+				await ctx.send('You do not have permission to edit that list.')
+				return
+			if column_name is None:
+				lists[list_name]['sort'] = None
+				await ctx.send('Sorting disabled.')
+				return
+			if column_name not in lists[list_name]['columns']:
+				await ctx.send('That column could not be found.')
+				return
+			idx = lists[list_name]['columns'].index(column_name)
+			lists[list_name]['sort'] = [idx, reverse]
+			await ctx.send(f'This list will now be sorted by column {column_name}.')
+		
+
+	@staticmethod
+	def _user_can_access(lm_list, user):
+		"""Determine if a user can edit a list."""
+		if lm_list['author'] == user.id:
+			return True
+		list_roles = lm_list.get('roles', [])
+		user_roles = (r.id for r in getattr(user, 'roles', []))
+		if any(rid in list_roles for rid in user_roles):
+			return True
+		return False
